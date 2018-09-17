f0b07c790f9ecabe84b8055d95f8d8567b2fe479
Steven Milov <sjm141@pitt.edu>
2018-09-17 14:11:30 -0400

    changed hover color

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // When the DOM is ready, run this function
 $(document).ready(function() {
-  
+
   //#HEADER
 	var slideHeight = $(window).height();
 	$('#headere-top figure .item').css('height',slideHeight);
@@ -8,38 +8,38 @@ $(document).ready(function() {
 	$(window).resize(function(){'use strict',
 		$('#headere-top figure .item').css('height',slideHeight);
 	});
-  
-  
-  
+
+
+
   //Scroll Menu
 	$(window).on('scroll', function(){
 		if( $(window).scrollTop()>600 ){
 			$('.header-top .header-fixed-wrapper').addClass('navbar-fixed-top animated fadeInDown');
-			
+
 		} else {
 			$('.header-top .header-fixed-wrapper').removeClass('navbar-fixed-top animated fadeInDown');
 		}
 	});
-	
-	
-	 $(window).scroll(function(){                          
+
+
+	 $(window).scroll(function(){
             if ($(this).scrollTop() > 200) {
                 $('#menu').fadeIn(500);
             } else {
                 $('#menu').fadeOut(500);
             }
         });
-	
+
 	// Navigation Scroll
 	$(window).scroll(function(event) {
 		Scroll();
 	});
 
-	$('.navbar-collapse ul li a').on('click', function() {  
+	$('.navbar-collapse ul li a').on('click', function() {
 		$('html, body').animate({scrollTop: $(this.hash).offset().top - 1}, 1000);
 		return false;
 	});
-	
+
 	// User define function
 	function Scroll() {
 		var contentTop      =   [];
@@ -55,11 +55,11 @@ $(document).ready(function() {
 			if ( winTop > contentTop[i] - rangeTop ){
 				$('.navbar-collapse li.scroll')
 				.removeClass('active')
-				.eq(i).addClass('active');			
+				.eq(i).addClass('active');
 			}
 		})
 	};
-  
+
   // affix
   var width = $(window).width();
   var top = $('.tp-banner-container').length == 0 ? -1 : $('.section-one').offset().top - $('.navbar').height() * 2;
@@ -71,11 +71,26 @@ $(document).ready(function() {
       }
     }
   });
-  
+//====================================added this to shrink logo
+/*  window.onscroll = function() {
+  growShrinkLogo()
+};
+
+function growShrinkLogo() {
+  var Logo = document.getElementById("danceEvolutionLogo")
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    Logo.style.width = '2em';
+    Logo.style.height = '2em';
+  } else {
+    Logo.style.width = '2em';
+    Logo.style.height = '2em';
+  }
+}
+*/
   var owl = $("#owl-demo");
 
       owl.owlCarousel({
-        
+
         itemsCustom : [
           [0, 1],
           [450, 1],
@@ -90,8 +105,8 @@ $(document).ready(function() {
 		autoPlay : 3000,
 
       });
-	  
-	  
+
+
 	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
           disableOn: 700,
           type: 'iframe',
@@ -101,5 +116,5 @@ $(document).ready(function() {
 
           fixedContentPos: false
         });
-  
+
 });
